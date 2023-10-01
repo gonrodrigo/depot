0366aa91acf918d850e63baecb70865752c875d5
Gonçalo Rodrigo <goncalor.ar@gmail.com>
2023-10-01 23:40:10 +0200

    [TASK-B] Validating and Unit Testing of Models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :title, length: { minimum: 10 }, uniqueness: true
+  validates :price,  numericality: { greater_than_or_equal_to: 0.01,
+    message: 'must be at least 1 cent.'
+  }
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
