3393695e70b67438a27dfd19df86194a5249d373
Gonçalo Rodrigo <goncalor.ar@gmail.com>
2023-12-09 23:08:11 +0000

    task-e-a-smarter-cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -15,15 +15,40 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create line_item" do
+  test "should create line_item with duplicate products" do
     assert_difference("LineItem.count") do
+      post line_items_url, params: { product_id: products(:ruby).id }
       post line_items_url, params: { product_id: products(:ruby).id }
     end
 
     follow_redirect!
 
-    assert_select "h2", "Your Pragmatic Cart"
-    assert_select "li", "Programming Ruby 1.9"
+    assert_select "h2", "Your Cart"
+    assert_select "td.quantity", "2"
+    assert_select "td.title", "Programming Ruby 1.9"
+  end
+
+  test "should create line_item with unique products" do
+    post line_items_url, params: { product_id: products(:ruby).id }
+    post line_items_url, params: { product_id: products(:two).id }
+
+    follow_redirect!
+
+    assert_select "h2", "Your Cart"
+    assert_select "td.quantity" do |elements|
+      assert_equal elements.length, 2
+      elements.each do |element|
+        assert_equal element.text, "1"
+      end
+    end
+
+    expected_titles = ["Programming Ruby 1.9", "My String 2"]
+    assert_select "td.title" do |elements|
+      assert_equal elements.length, 2
+      elements.each_with_index do |element, index|
+        assert_equal element.text, expected_titles[index]
+      end
+    end
   end
 
   test "should show line_item" do
@@ -37,7 +62,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item), params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
